449ef4bcf418bccd5064a6b08e1f7a66742e0ad4
Anderson Figueredo Gomes <gomes.ad@gmail.com>
2020-05-10 21:49:17 -0300

    First Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactions = this.all();
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((total, obj) => total + obj.value, 0);
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((total, obj) => total + obj.value, 0);
+    const totalBalance = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total: totalBalance,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
